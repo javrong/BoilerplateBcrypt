12c62c7c124dd9286e5431c290077433ae77d50d
javrong <sunumd@yahoo.com>
2021-12-13 09:37:08 +0700

    First initial

---
+++ server.js
@@ -0,0 +1,68 @@
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt      = require('bcrypt');
+const app         = express();
+fccTesting(app);
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+
+
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err,hash) => {});
+
+bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+  res == true
+})
+
+bcrypt.hash('passwd', 13, (err, hash) => {
+  console.log(hash);
+
+  bcrypt.compare('passwd', hash, (err, res) => {
+    console.log(res);
+  });
+});
+
+//END_ASYNC
+
+//START_SYNC
+
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+
+//END_SYNC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, () => {});
